b27890983e8281f68dcf9833e1a9497beea541a9
Bereket Negash <negash.bereket.se@gmail.com>
2024-11-05 22:52:19 +0400

    handlers

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 const path = require('path')
 const express = require('express')
+const { title } = require('process')
+//const hbs = require('hbs')
 
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'My Message For You',
+        body: 'Jesus Christ is Our Lord and Savior , He saved us from eternal destruction, may his name be blessed for ever and ever, AMen!'
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Bereket Negash'
+    })
+})
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: ' Bekako'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
