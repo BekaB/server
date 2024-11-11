cf241f1f579575a62892875f96be661d0985927b
Bereket Negash <negash.bereket.se@gmail.com>
2024-11-11 22:11:21 +0400

    client side fetching

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
 console.log('Client side javascript file is loaded!')
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    fetch('https://api.weatherstack.com/current?access_key=a818ae5bf8e13c72847c794148f22f96&query='+location).then((response)=>{
+        response.json().then((data) => {
+            if(data.error){
+                return console.log(data.error)
+            }else{
+            console.log(data.location.name)
+            console.log(data.current.weather_descriptions[0])
+        }
+    
+        })
+    })
+})
