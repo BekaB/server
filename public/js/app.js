aebbcdf972e60c0ccf1d6e9dbfb1b5414a035874
Bereket Negash <negash.bereket.se@gmail.com>
2024-11-14 05:13:01 +0400

    heroku setup

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 console.log('Client side javascript file is loaded!')
 
 
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#messageOne')
@@ -17,18 +16,16 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading'
     messageTwo.textContent = ''
 
-    fetch('https://api.weatherstack.com/current?access_key=a818ae5bf8e13c72847c794148f22f96&query='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
         response.json().then((data) => {
             if(data.error){
                 return messageOne.textContent = 'Unable to Find Location , Please try Another Search'
             }else{
-
                 messageOne.textContent = data.location.name
                 messageTwo.textContent = data.current.weather_descriptions[0]
             //console.log(data.location.name)
             //console.log(data.current.weather_descriptions[0])
-        }
-    
+            }
         })
     })
 })
